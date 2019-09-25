b68ae82cc12ea6727fe82c8c7cca777f0c322c33
sanju tamang <sanjutamang138@gmail.com>
2019-09-25 10:32:08 +0530

    servingstatichtml

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const express=require('express');
 const http=require('http');
+const morgan=require('morgan');
 const hostname="localhost";
 const portno=3000;
-const app=express();
 
+
+
+const app=express();
+app.use(morgan('dev'));
+app.use(express.static(__dirname+'/public'));
 app.use((req,res,next)=>
 {
     console.log(req.headers);
-    res.statusCode=200;
+    
     res.setHeader('content-type','text/html');
     res.end('<html><body><h1>This is an Express Server written by sanju</h1></body></html>');
 });
